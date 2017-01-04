862c9dc1f4a48faeedb2eff1b583f3d009e0c32a
Sami Bhatti <abdulsami.bhatti@gmail.com>
2017-01-04 16:47:02 +0100

    Update VendorRepositoryTests.cs

--- AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
+++ AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Acme.Biz;
 using System;
 using System.Collections.Generic;
@@ -11,6 +11,66 @@ namespace Acme.Biz.Tests
     [TestClass()]
     public class VendorRepositoryTests
     {
+        [TestMethod()]
+        public void RetrieveValueTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = 42;
 
+            //Act
+            var actual = repository.RetrieveValue<int>("Select ...", 42);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveValueStringTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var expected = "test";
+
+            //Act
+            var actual = repository.RetrieveValue<string>("Select ...", "test");
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveValueObjectTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            var vendor = new Vendor();
+            var expected = vendor;
+
+            //Act
+            var actual = repository.RetrieveValue<Vendor>("Select ...", vendor);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RetrieveTest()
+        {
+            //Arrange
+            var repository = new VendorRepository();
+            //var expected = 2;
+            var expected = new List<Vendor>();
+            expected.Add(new Vendor()
+            { VendorId = 1, CompanyName = "ABC Corp", Email = "abc@abc.com" });
+            expected.Add(new Vendor()
+            { VendorId = 2, CompanyName = "XYZ Inc", Email = "xyz@xyz.com" });
+            //Act
+            var actual = repository.Retrieve();
+
+            //Assert
+           // Assert.AreEqual(expected, actual.Count);
+            CollectionAssert.AreEqual(expected, actual);          
+        }
     }
 }
